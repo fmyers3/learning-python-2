31b4baf14dbbb5da000dfc29b83325b4df2e859f
Fallon Myers <fmyers@gmail.com>
2022-08-31 07:44:57 +0000

    final_list2

---
+++ Lists2_wk12.py
@@ -0,0 +1,24 @@
+aws_services = []
+
+print(len(aws_services))
+
+aws_services = ['EC2', 'Python', 'Lambda']
+
+print(aws_services)
+
+print (len(aws_services)) 
+
+aws_services.append('s3')
+
+aws_services.insert(0,'DynamoDB')
+
+print(aws_services)
+
+print (len(aws_services))
+
+aws_services.remove('s3')
+aws_services.remove('Lambda')
+
+print (len(aws_services))
+
+print(aws_services)
